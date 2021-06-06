7dc010f579012f9f8e64ebdb76244f0bdd143889
Sonya Chan <sonya@jewlr.com>
2021-06-05 23:36:30 -0400

    Initialize project with styled-components, eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+const base = require("./.eslintrc.base.json")
+
+base.rules["import/no-unresolved"] = ["error"]
+
+module.exports = base
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import styled from "styled-components"
+
+import styles from "../styles/Home.module.css"
+
+const Heading = styled.h1`
+  color: green;
+  font-size: 44px;
+`
 
 export default function Home() {
   return (
@@ -12,12 +19,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Heading>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </Heading>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,7 +65,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
